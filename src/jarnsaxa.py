35e61c917f59485ee241bf23df017da62e6a8639
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2025-08-18 11:13:31 -0600

    Packable: Added from graf

--- src/jarnsaxa.py
+++ src/jarnsaxa.py
@@ -5,6 +5,165 @@ import h5py
 import time
 import json
 import numpy as np
+from abc import ABC, abstractmethod
+import pylogfile as plf
+from colorama import Fore, Style
+import copy
+
+class Packable(ABC):
+	""" This class represents all objects that can be packed and unpacked and sent between the client and server
+	
+	manifest, obj_manifest, and list_template are all dictionaries. Each describes a portion of how to represent a class as a string,
+	and how to convert back to the class from the string data.
+	
+	manifest: lists all variables that can be converted to/from JSON natively
+	obj_manifest: lists all variables that are Packable objects or lists of packable objects. Each object will have
+		pack() called, and be understood through its unpack() function.
+	list_template: dictionary mapping item to pack/unpack to its class type, that way during unpack, Packable knows which
+		class to create and call unpack() on.
+	
+	Populate all three of these variables as needed in the set_manifest function. set_manifest is called in super().__init__(), so
+	it shouldn't need to be remembered in any of the child classes.
+	"""
+	
+	def __init__(self, log:plf.LogPile=None):
+		
+		if log is None:
+			print(f"{Fore.RED}Log object was not initialized in {type(self)} instance.!{Style.RESET_ALL}")
+			self.log = plf.LogPile(use_mutex=False)
+		else:
+			self.log = log
+			self.log.set_enable_mutex(False)
+		
+		self.log.lowdebug(f"Created object type={type(self)}")
+		self.manifest = []
+		self.obj_manifest = []
+		self.list_manifest = {}
+		self.dict_manifest = {}
+		
+		self.set_manifest()
+	
+	@abstractmethod
+	def set_manifest(self):
+		""" This function will populate the manifest and obj_manifest objects"""
+		pass
+	
+	def pack(self):
+		""" Returns the object to as a JSON dictionary """
+		
+		# Initialize dictionary
+		d = {}
+		
+		# Add items in manifest to packaged data
+		for mi in self.manifest:
+			d[mi] = getattr(self, mi)
+		
+		# Scan over object manifest
+		for mi in self.obj_manifest:
+			# Pack object and add to output data
+			try:
+				d[mi] = getattr(self, mi).pack()
+			except:
+				raise Exception(f"'Packable' object had corrupt object manifest item '{mi}'. Cannot pack.", detail=f"Type = {type(self)}")
+				
+		# Scan over list manifest
+		for mi in self.list_manifest:
+				
+			# Pack objects in list and add to output data
+			d[mi] = [x.pack() for x in getattr(self, mi)]
+		
+		# Scan over dict manifest
+		for mi in self.dict_manifest:
+			
+			mi_deref = getattr(self, mi)
+			
+			# Pack objects in dict and add to output data
+			d[mi] = {}
+			for midk in mi_deref.keys():
+				d[mi][midk] = mi_deref[midk].pack()
+				
+		# Return data list
+		return d
+	
+	def unpack(self, data:dict):
+		""" Populates the object from a JSON dict """
+		
+		# Try to populate each item in manifest
+		for mi in self.manifest:
+			self.log.lowdebug(f"Unpacking manifest, item:>{mi}<")
+			
+			# Try to assign the new value
+			try:
+				setattr(self, mi, data[mi])
+			except Exception as e:
+				self.log.error(f"Failed to unpack item in object of type '{type(self).__name__}'. ({e})", detail=f"Type = {type(self)}")
+				return
+		
+		# Try to populate each Packable object in manifest
+		for mi in self.obj_manifest:
+			self.log.lowdebug(f"Unpacking obj_manifest, item:>{mi}<")
+			
+			# Try to update the object by unpacking the item
+			try:
+				getattr(self, mi).unpack(data[mi])
+			except Exception as e:
+				self.log.error(f"Failed to unpack Packable in object of type '{type(self).__name__}'. ({e})", detail=f"Type = {type(self)}")
+				return
+			
+		# Try to populate each list of Packable objects in manifest
+		for mi in self.list_manifest.keys():
+				
+			# Scan over list, unpacking each element
+			temp_list = []
+			for list_item in data[mi]:
+				self.log.lowdebug(f"Unpacking list_manifest, item:>{mi}<, element:>:a{list_item}<")
+				
+				# Try to create a new object and unpack a list element
+				try:
+					# Create a new object of the correct type
+					new_obj = copy.deepcopy(self.list_manifest[mi])
+					
+					# Populate the new object by unpacking it, add to list
+					new_obj.unpack(list_item)
+					temp_list.append(new_obj)
+				except Exception as e:
+					self.log.error(f"Failed to unpack list of Packables in object of type '{type(self).__name__}'. Type={type(self)}. ({e})", detail=f"Type = {type(self)}")
+					return
+			setattr(self, mi, temp_list)
+				# self.obj_manifest[mi] = copy.deepcopy(temp_list)
+		
+		# Scan over dict manifest
+		for mi in self.dict_manifest.keys():
+			
+			# mi_deref = getattr(self, mi)
+			
+			# # Pack objects in list and add to output data
+			# d[mi] = [mi_deref[midk].pack() for midk in mi_deref.keys()]
+			
+			# Scan over list, unpacking each element
+			temp_dict = {}
+			for dmk in data[mi].keys():
+				self.log.lowdebug(f"Unpacking manifest, item:>{mi}<, element:>:a{dmk}<")
+				
+				# Try to create a new object and unpack a list element
+				try:
+					# Create a new object of the correct type
+					try:
+						new_obj = copy.deepcopy(self.dict_manifest[mi])
+					except Exception as e:
+						print("Dict Manifest:")
+						print(self.dict_manifest)
+						self.log.error(f"Failed to unpack dict_manifest[{mi}], ({e})")
+						return
+					
+					# Populate the new object by unpacking it, add to list
+					new_obj.unpack(data[mi][dmk])
+					temp_dict[dmk] = new_obj
+				except Exception as e:
+					prob_item = data[mi][dmk]
+					self.log.error(f"Failed to unpack dict of Packables in object of type '{type(self).__name__}'. ({e})", detail=f"Class={type(self)}, problem manifest item=(name:{dmk}, type:{type(prob_item)})")
+					return
+			setattr(self, mi, temp_dict)
 
 def wrap_text(text:str, width:int=80):
 	""" Accepts a string, and wraps it over multiple lines. Honors line breaks. Returns a single string."""
